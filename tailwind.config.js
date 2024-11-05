48d6b76bf448531838e55ae8ae5cfa11f994a7b3
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-11-06 01:14:59 +0530

    Added Image as background and Added welcome heading

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,13 @@ module.exports = {
       fontFamily: {
         inter: ["'Inter'", "sans-serif"],
         gelasio: ["'Gelasio'", "serif"],
-        lato:["'Lato'","sans-serif"],
-        merriweather:["'Merriweather'","serif"]
+        lato: ["'Lato'", "sans-serif"],
+        merriweather: ["'Merriweather'", "serif"]
       },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#60a5fa", // blue-400 hex value
       },
     },
   },
