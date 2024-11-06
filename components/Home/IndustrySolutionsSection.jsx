bf88bf18f389eeedb4b138c81090ce034c4ca49d
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-11-06 11:37:16 +0530

    completed IndustrySolutionsSection

--- components/Home/IndustrySolutionsSection.jsx
+++ components/Home/IndustrySolutionsSection.jsx
@@ -12,21 +12,21 @@ const IndustrySolutionsSection = () => {
     ];
 
     return (
-        <div className="bg-black/60 pt-20 text-white flex flex-col space-y-7">
-            <h1 className="heading">Transforming Industries with AI-Driven Drone Solution</h1>
+        <div className="flex flex-col pb-20">
+            <h1 className="heading text-primary">Transforming Industries with AI-Driven Drone Solution</h1>
             <p className="tagline">
                 From defense and security to agriculture and environmental conservation, Qoptars’ autonomous drones and AI analytics deliver essential insights tailored to industry-specific needs. Our technology is transforming:
             </p>
-            <div className="flex flex-wrap justify-center space-x-4">
+            <div className="flex flex-wrap justify-around bg-none shadow-xl">
                 {industries.map((industry, index) => (
-                    <div key={index} className="bg-white p-6 rounded-md center flex-col m-2 w-40">
-                        <FontAwesomeIcon icon={industry.icon} className="text-xl mb-2 h-10 w-10" />
-                        <h2 className="font-light">{industry.name}</h2>
+                    <div key={index} className="p-6 rounded-md center flex-col m-2 space-y-3 border border-primary hover:bg-primary/60 w-44">
+                        <FontAwesomeIcon icon={industry.icon} className="mb-2 h-10 w-10 bg-transparent" />
+                        <h2 className="font-medium text-center bg-transparent">{industry.name}</h2>
                     </div>
                 ))}
             </div>
-            <div className="text-center mt-8">
-                <button className="bg-blue-500 text-white px-6 py-2 rounded-full font-semibold">
+            <div className="text-center my-12">
+                <button className="btn text-white px-6 py-2">
                     See Our Industry Solutions
                 </button>
             </div>
