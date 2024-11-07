22bfb6304962661bf6a28b62575e7e577cc96876
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-11-07 20:35:00 +0530

    Completed InTheNews Section

--- components/Home/InTheNewsSection.jsx
+++ components/Home/InTheNewsSection.jsx
@@ -3,21 +3,22 @@ import React from 'react'
 const InTheNewsSection = () => {
      // Array of image sources and alt texts for each news logo
     const newsLogos = [
-        { src: "/path/to/image1.png", alt: "CNN Logo" },
-        { src: "/path/to/image2.png", alt: "Disney Channel Logo" },
-        { src: "/path/to/image3.png", alt: "ESPN Logo" },
-        { src: "/path/to/image4.png", alt: "Cinemax Logo" }
+        { src: "/images/InTheNews/TimesOfIndia.png", alt: "TimesOfIndia" },
+        { src: "/images/InTheNews/ET.png", alt: "ET" },
+        { src: "/images/InTheNews/INC42.png", alt: "INC42" },
+        { src: "/images/InTheNews/dailyhunt.png", alt: "dailyhunt" }
     ];
     return (
-        <div>
-            <h1 className="heading pb-10">In The News</h1>
-            <div className="grid grid-cols-2 sm:grid-cols-4 gap-6 px-10 md:px-20">
+        <div className="pb-32">
+            <h1 className="heading">In The News</h1>
+            <p className="tagline text-xl">Limelight we obviously expected and loved</p>
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-10 px-10 md:px-20">
                 {newsLogos.map((logo, index) => (
                     <div 
                         key={index} 
-                        className="p-6 bg-white/5 rounded-lg flex justify-center items-center shadow-md hover:shadow-lg transition-shadow"
+                        className="p-7 bg-white/5 rounded-lg flex justify-center items-center shadow-md hover:shadow-lg transition-shadow"
                     >
-                        <img src={logo.src} alt={logo.alt} className="h-12 md:h-16 object-contain" />
+                        <img src={logo.src} alt={logo.alt} className="h-12 md:h-20 object-contain bg-transparent" />
                     </div>
                 ))}
             </div>
