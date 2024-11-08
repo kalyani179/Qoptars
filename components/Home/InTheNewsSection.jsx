a3d9de63b46ddd5d061e55c588c361526c1d043b
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-11-08 12:29:43 +0530

    Added effects

--- components/Home/InTheNewsSection.jsx
+++ components/Home/InTheNewsSection.jsx
@@ -1,4 +1,6 @@
+"use client"
 import React from 'react'
+import { Slide, Zoom,Fade } from "react-awesome-reveal";
 
 const InTheNewsSection = () => {
      // Array of image sources and alt texts for each news logo
@@ -10,16 +12,19 @@ const InTheNewsSection = () => {
     ];
     return (
         <div className="pb-32">
-            <h1 className="heading">In The News</h1>
-            <p className="tagline text-xl">Limelight we obviously expected and loved</p>
-            <div className="grid grid-cols-2 sm:grid-cols-4 gap-10 px-10 md:px-20">
+            <Zoom duration={1000}>
+                <h1 className="heading">In The News</h1>
+            </Zoom>
+            <Slide direction='up' duration={1000}>
+                <p className="tagline text-xl">Limelight we obviously expected and loved</p>
+            </Slide>
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-10 px-10 pt-10 md:px-20">
                 {newsLogos.map((logo, index) => (
-                    <div 
-                        key={index} 
-                        className="p-7 bg-white/5 rounded-lg flex justify-center items-center shadow-md hover:shadow-lg transition-shadow"
-                    >
+                    <Fade key={index} duration={2000}>
+                    <div  className="p-7 bg-white/10 rounded-lg flex justify-center items-center shadow-md hover:shadow-lg transition-shadow">
                         <img src={logo.src} alt={logo.alt} className="h-12 md:h-20 object-contain bg-transparent" />
                     </div>
+                    </Fade>
                 ))}
             </div>
         </div>
--- components/Home/IndustrySolutionsSection.jsx
+++ components/Home/IndustrySolutionsSection.jsx
@@ -1,6 +1,8 @@
+"use client"
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShieldAlt, faIndustry, faBolt, faLeaf, faCity } from '@fortawesome/free-solid-svg-icons';
+import { Slide, Zoom,Fade } from "react-awesome-reveal";
 
 const IndustrySolutionsSection = () => {
     const industries = [
@@ -13,23 +15,35 @@ const IndustrySolutionsSection = () => {
 
     return (
         <div className="flex flex-col pb-20">
-            <h1 className="heading text-4xl">Transforming Industries with AI-Driven Drone Solution</h1>
-            <p className="tagline">
-                From defense and security to agriculture and environmental conservation, Qoptars’ autonomous drones and AI analytics deliver essential insights tailored to industry-specific needs. Our technology is transforming:
-            </p>
-            <div className="flex flex-wrap justify-around">
+        <div className="bg-black pt-20">
+            <Zoom duration={2000}>
+                <h1 className="heading text-4xl">Transforming Industries with AI-Driven Drone Solution</h1>
+            </Zoom>  
+        </div>
+        <div className="bg-black">
+            <Slide direction='up' duration={1000}>
+                <p className="tagline">
+                    From defense and security to agriculture and environmental conservation, Qoptars’ autonomous drones and AI analytics deliver essential insights tailored to industry-specific needs. Our technology is transforming:
+                </p>
+            </Slide>
+            </div>
+            <div className="flex flex-wrap justify-around pt-5">
                 {industries.map((industry, index) => (
-                    <div key={index} className="p-6 rounded-md center flex-col m-2 space-y-3 border border-primary hover:bg-primary/60 w-44">
-                        <FontAwesomeIcon icon={industry.icon} className="mb-2 h-10 w-10 bg-transparent" />
-                        <h2 className="font-medium text-center bg-transparent">{industry.name}</h2>
-                    </div>
+                    <Zoom key={index} duration={2000}>
+                        <div className="p-6 rounded-md center flex-col m-2 space-y-3 border border-primary hover:bg-primary/60 w-44">
+                            <FontAwesomeIcon icon={industry.icon} className="mb-2 h-10 w-10 bg-transparent" />
+                            <h2 className="font-medium text-center bg-transparent">{industry.name}</h2>
+                        </div>
+                    </Zoom>
                 ))}
             </div>
+            <Fade duration={3000}>
             <div className="text-center my-12">
                 <button className="btn text-white px-6 py-2">
                     See Our Industry Solutions
                 </button>
             </div>
+            </Fade>
         </div>
     );
 };
