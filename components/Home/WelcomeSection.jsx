5cc92247252119c6641e10c61209346c48d98062
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-11-08 12:35:37 +0530

    modified effects

--- components/Home/WelcomeSection.jsx
+++ components/Home/WelcomeSection.jsx
@@ -12,17 +12,17 @@ const WelcomeSection = () => {
         <div className="absolute inset-0 bg-black/60 max-h-screenx">
             <Navbar />
             <div className="center flex-col sm:gap-2 md:mt-9 sm:mt-6">
-                <Zoom duration={1000}>
+                <Zoom duration={2000}>
                     <h1 className={`text-2xl md:text-5xl lg:text-6xl xl:text-6xl font-extrabold mt-5 md:mt-24 lg:mt-28 xl:mt-36 tracking-wider text-center`}>
                         Welcome to <span className="text-2xl md:text-5xl lg:text-6xl xl:text-6xl font-extrabold ml-2 text-primary tracking-wide">Qoptars</span>
                     </h1>
                 </Zoom>
-                <Slide direction='up' duration={1000}>
+                <Slide direction='up' duration={2000}>
                     <p className={`text-base mt-3 sm:text-xs md:text-lg lg:text-xl font-semibold tracking-widest uppercase text-center opacity-80`}>
                     Redefining Aerial Intelligence and Autonomous Drones with AI.
                     </p>
                 </Slide>
-                <Fade delay={500}>
+                <Fade delay={1000}>
                     <button className="btn px-5 rounded-md mt-14 tracking-wide">
                     Learn More About Our Technology
                     </button>
