20735e12ec03f5e56f0a878ef0e057902c5bcad1
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-11-30 17:47:04 +0530

    completed responsiveness of home page

--- components/Home/Navbar.jsx
+++ components/Home/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
                     <ul className="hidden md:flex space-x-10">
                         <li className="nav-link"><a href="/">Home</a></li>
                         <li className="nav-link"><a href="/about">About Us</a></li>
-                        <li className="nav-link">Ai Ecosystem</li>
-                        <li className="nav-link">Products</li>
-                        <li className="nav-link">News</li>
+                        <li className="nav-link"><a href="/ai-ecosystem">Ai Ecosystem</a></li>
+                        <li className="nav-link"><a href="/products">Products</a></li>
+                        <li className="nav-link"><a href="/news">News</a></li>
                     </ul>
                     <button className="hidden md:block bg-white text-black py-2 px-6 rounded-lg shadow hover:bg-white/80">
                         Contact us
@@ -36,7 +36,7 @@ const Navbar = () => {
                 >
                     {isOpen ? (
                         <svg
-                            className="w-6 h-6 text-black"
+                            className="w-6 h-6 text-black z-50"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
@@ -51,7 +51,7 @@ const Navbar = () => {
                         </svg>
                     ) : (
                         <svg
-                            className="w-6 h-6 text-black"
+                            className="w-6 h-6 text-white"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
@@ -70,26 +70,18 @@ const Navbar = () => {
 
             {/* Sidebar Menu for Small Screens */}
             <div
-                className={`fixed top-0 right-0 h-full bg-white shadow-md w-2/4 transform transition-transform duration-300 ${
+                className={`fixed z-40 top-0 right-0 h-full bg-white shadow-md w-2/4 transform transition-transform duration-300 ${
                     isOpen ? "translate-x-0" : "translate-x-full"
                 }`}
             >
                 <ul className="flex flex-col items-center space-y-10 py-20">
-                    <li className="navlink">
-                        Solutions
-                    </li>
-                    <li className="navlink">
-                        Industries
-                    </li>
-                    <li className="navlink">
-                        About
-                    </li>
-                    <li className="navlink">
-                        Contact
-                    </li>
-                    <button className="bg-black text-white py-2 px-6 rounded-lg shadow hover:bg-white/80">
-                        Get Started
-                    </button>
+                    <a href="/" className="nav-link">Home</a>
+                    <a href="/about" className="nav-link">About Us</a>
+                    <a href="/ai-ecosystem" className="nav-link">AI Ecosystem</a>
+                    <a href="/news" className="nav-link">News</a>
+                    <a href="/products" className="nav-link">Products</a>
+                    <a href="/news" className="nav-link">News</a>
+                    <a href="/contact-us" className="block py-2 px-4 text-white bg-black text-center rounded-full my-2 mx-4 hover:bg-gray-200">Contact Us</a>
                 </ul>
             </div>
         </div>
